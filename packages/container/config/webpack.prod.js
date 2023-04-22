fa5bb40a7c0f1111ebe6a3550a0d63ed57e80325
o.lopez <o.lopez@draftkings.com>
2023-04-22 22:14:04 +0300

    container production config build

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederation = require('webpack/lib/container/ModuleFederationPlugin');
+
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const productConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederation({
+            name: 'container',
+            remotes: {
+                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, productConfig);
+
