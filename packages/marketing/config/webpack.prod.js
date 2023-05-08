fecd4551288b735073c9b39e0165b2516f8cd1ee
o.lopez <o.lopez@draftkings.com>
2023-05-08 16:18:10 +0300

    create webpack config for prod deployment of marketing app

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const ModuleFederation = require('webpack/lib/container/ModuleFederationPlugin');
+
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const productConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest'
+    },
+    plugins: [
+        new ModuleFederation({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './Marketing': './src/boostrap'
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, productConfig);
